46617aff4b2c91b32b825ffca385e4a93541ccc8
jashezan <84038649+jashezan@users.noreply.github.com>
2024-02-23 19:48:18 +0600

    adding search input fields, videos and images

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,8 @@ import heroVideo from '../assets/images/hero-video.mp4'
 import worldImg from '../assets/images/world.png'
 import Subtitle from './../shared/Subtitle'
 
+import SearchBar from './../shared/SearchBar'
+
 const Home = () => {
   return <>
   
@@ -25,6 +27,24 @@ const Home = () => {
             <p>“Embrace the journey, for it is the path that shapes you. Every step, every challenge, every setback—it all contributes to your growth. You are stronger than you realize, and your potential knows no bounds. Keep moving forward, even when the road seems steep. The view from the top will be worth every effort.”</p>
           </div>
         </Col>
+
+        <Col lg='2'>
+          <div className="hero__img-box">
+            <img src={heroImg} alt="" />
+          </div>
+        </Col>
+        <Col lg='2'>
+          <div className="hero__img-box mt-4">
+            <video src={heroVideo} alt="" controls />
+          </div>
+        </Col>
+        <Col lg='2'>
+          <div className="hero__img-box mt-5">
+            <img src={heroImg02} alt="" />
+          </div>
+        </Col>
+
+        <SearchBar />
       </Row>
     </Container>
   </section>
