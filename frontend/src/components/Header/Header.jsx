a86ca2fcce69927c68f32b41665ad3b88820bd41
jashezan <84038649+jashezan@users.noreply.github.com>
2024-02-23 16:34:58 +0600

    added shared folder

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
 
           <div className="nav__right d-flex align-items-center gap-4">
             <div className="nav__btns d-flex align-items-center gap-4">
-              <Button className='btn secondary__btn'><Link to='/login'>Login_nowgi</Link></Button>
+              <Button className='btn secondary__btn'><Link to='/login'>Login</Link></Button>
               <Button className='btn primary__btn'><Link to='/register'>Register</Link></Button>
             </div>
 
---
+++ frontend/src/shared/Subtitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Subtitle = ({subtitle}) => {
+  return (
+    <h3 className='section__subtitle'>{subtitle}</div>
+  )
+}
+
+export default Subtitle
