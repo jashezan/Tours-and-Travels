5b5eb9999658dc5eb2acc5fdb485981704c3960d
jashezan <84038649+jashezan@users.noreply.github.com>
2024-02-23 16:52:11 +0600

    added some content on homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import '../styles/home.css'
+
+import { Container, Row,Col } from 'reactstrap'
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import Subtitle from './../shared/Subtitle'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <>
+  
+  {/*============== hero section start =============== */}
+  <section>
+    <Container>
+      <Row>
+        <Col lg='6'>
+          <div className="hero__content">
+            <div className="hero__subtitle d-flex align-items-center ">
+              <Subtitle subtitle={'Learn Before You Leap'}/>
+              <img src={worldImg} alt="" />
+            </div>
+            <h1>Traveling opens the door to creating <span className="highlight">memories</span></h1>
+            <p>“Embrace the journey, for it is the path that shapes you. Every step, every challenge, every setback—it all contributes to your growth. You are stronger than you realize, and your potential knows no bounds. Keep moving forward, even when the road seems steep. The view from the top will be worth every effort.”</p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  </section>
+  {/*============== hero section end =============== */}
+
+  </>
 }
 
 export default Home
