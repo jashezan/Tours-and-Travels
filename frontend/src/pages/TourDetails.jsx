2e79e64f7a0ca73851a76033922b4e9cb252bb14
jashezan <84038649+jashezan@users.noreply.github.com>
2024-02-27 20:33:32 +0600

    added commonsection and some tour-detail page functionality

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import '../styles/tour-details.css'
+import { Container, Row, Col, Form, ListGroup } from 'reactstrap'
+import { useParams } from 'react-router-dom'
+import tourData from '../assets/data/tours'
+import calculateAvgRating from './../utils/avgRating'
 
 const TourDetails = () => {
+
+  const { id } = useParams()
+
+  // currently using static data, to use api later and fetch data from database
+  const tour = tourData.find(tour => tour.id === id)
+
+  // destructure properties from tour object
+  const { photo, title, desc, price, address, reviews, city, distance, maxGroupSize } = tour
+
+  const { totalRating, avgRating } = calculateAvgRating(reviews)
+
   return (
-    <div>TourDetails</div>
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='8'>
+              <div className="tour__content">
+                <img src={photo} alt="" />
+                <div className="tour__info">
+                  <h2>{title}</h2>
+
+                  <div className="d-flex align-items-center gap-5">
+                    <span className='tour__rating d-flex alignt-items center gap-1'>
+                      <i class="ri-star-fill" style={{ 'color': 'var(--secondary-color)' }}></i> {avgRating === 0 ? null : avgRating}
+                      {totalRating === 0 ? (
+                        'Not rated'
+                      ) : (
+                        <span>({reviews?.length})</span>
+                      )}
+                    </span>
+
+                    <span>
+                      <i className="ri-map-pin-user-fill"></i> {address}
+                    </span>
+
+                  </div>
+                  <div className="tour__extra-details">
+                    <span>
+                      <i className="ri-map-pin-2-line"></i> {city}
+                    </span>
+                    <span>
+                      <i className="ri-money-dollar-circle-line"></i> {price} / person
+                    </span>
+                    <span>
+                      <i className="ri-group-line"></i> {maxGroupSize}
+                    </span>
+                  </div>
+
+                  <h5>Description</h5>
+                  <p>{desc}</p>
+
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   )
 }
 
