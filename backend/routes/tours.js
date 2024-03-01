e241b43391202fe1ae8dc87d189c0b2f7c9ac7d0
jashezan <84038649+jashezan@users.noreply.github.com>
2024-03-01 20:29:56 +0600

    attemtping to intergrate newer backend programs

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -3,26 +3,34 @@ import {
   createTour, 
   deleteTour, 
   getAllTour, 
+  getFeaturedTour, 
   getSingleTour, 
+  getTourBySearch, 
   updateTour,
+  getTourCount,
 } from './../controllers/tourController.js';
 
-const router = express.Router()
+const router = express.Router();
 
 // create new tour
 router.post('/', createTour);
 
 // update new tour
-router.put('/:id', updateTour)
+router.put('/:id', updateTour);
 
 // delete new tour
-router.post('/:id', deleteTour)
+router.delete('/:id', deleteTour);
 
 // get single tour
-router.get('/:id', getSingleTour)
+router.get('/:id', getSingleTour);
 
-// getAll new tour
-router.get('/:id', getAllTour)
+// get All tour
+router.get('/', getAllTour);
 
- 
+
+//get tour by search
+router.get("/search/getTourBySearch", getTourBySearch);
+router.get("/search/getFeaturedTours", getFeaturedTour);
+router.get("/search/getTourCount", getTourCount);
+
 export default router;
