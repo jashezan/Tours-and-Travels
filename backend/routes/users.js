367418453e9c2a043004c204841b8adf78f71ec3
jashezan <84038649+jashezan@users.noreply.github.com>
2024-03-01 21:19:53 +0600

    previous was wrong backend, now attempting correct backend folder

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,16 +7,18 @@ import {
 } from '../controllers/userController.js';
 const router = express.Router()
 
+import {verifyAdmin,verifyUser} from "../utils/verifyToken.js"
+
 // update new User
-router.put('/:id', updateUser);
+router.put('/:id', verifyUser, updateUser);
 
 // delete new User
-router.delete('/:id', deleteUser);
+router.delete('/:id', verifyUser, deleteUser);
 
 // get single User
-router.get('/:id', getSingleUser);
+router.get('/:id', verifyUser , getSingleUser);
 
 // get All User
-router.get('/', getAllUser);
+router.get('/', verifyAdmin,getAllUser);
 
-export default router
+export default router;
--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,15 +1,21 @@
-
-
 import React from 'react'
 import TourCard from '../../shared/TourCard'
 import tourData from '../../assets/data/tours'
 import { Col } from 'reactstrap'
 
+import useFetch from './../../hooks/useFetch'
+import { BASE_URL } from './../../utils/config'
+
 const FeaturedTourList = () => {
-  return (
+    const { data: featuredTours } = useFetch(
+        `${BASE_URL}/tours/search/getFeaturedTours`
+    )
+
+    console.log(featuredTours)
+    return (
         <>
             {tourData?.map(tour => (
-                <Col lg='3' className='mb-4' key={tour.id}>
+                <Col lg='3' className='mb-4' key={tour._id}>
                     <TourCard tour={tour} />
                 </Col>
             ))}
