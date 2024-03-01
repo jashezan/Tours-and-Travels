32855ab56e48e1a45ffbb0be7415ebdd152230d7
jashezan <84038649+jashezan@users.noreply.github.com>
2024-03-01 21:53:52 +0600

    fixing mistakes

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,12 +1,13 @@
-
-
 import React from 'react'
 import TourCard from '../../shared/TourCard'
 import tourData from '../../assets/data/tours'
 import { Col } from 'reactstrap'
 
+import useFetch from '../../hooks/useFetch'
+import { BASE_URL } from 
+
 const FeaturedTourList = () => {
-  return (
+    return (
         <>
             {tourData?.map(tour => (
                 <Col lg='3' className='mb-4' key={tour.id}>
