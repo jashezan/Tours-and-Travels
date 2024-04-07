e0221c6ef37c8ec7fa5efee098863d8958745465
jashezan <84038649+jashezan@users.noreply.github.com>
2024-04-07 08:45:39 +0600

    Moved Frontend to Vite from create-react-app for faster renderring

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import "./styles/App.css";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return <Layout />;
+}
+
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,13 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./context/ChatProvider";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <ChatProvider>
     <AuthContextProvider>
       <BrowserRouter>
